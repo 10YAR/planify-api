cec8142c253e9c4cdd2bf7581954f92c63a28a23
Thomasevano <thomas-evano@hotmail.fr>
2023-02-24 10:36:49 +0100

    adjust AppointmentTime validate length

--- api/types/appointment.type.go
+++ api/types/appointment.type.go
@@ -4,7 +4,7 @@ type Appointment struct {
 	ID                  int    `validate:"number" json:"id"`
 	CustomerName        string `validate:"required,min=3,max=32" json:"customer_name"`
 	AppointmentDate     string `validate:"required,len=10" json:"appointment_date"`
-	AppointmentTime     string `validate:"required,len=8" json:"appointment_time"`
+	AppointmentTime     string `validate:"required,min=7,max=8" json:"appointment_time"`
 	AppointmentDateTime string `validate:"" json:"appointment_date_time"`
 	ShopId              int    `validate:"required,number" json:"shop_id"`
 }
