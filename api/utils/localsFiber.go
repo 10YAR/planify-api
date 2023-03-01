40f3ab5386193faf7a697386e8503908ece2a33c
Thomasevano <thomas-evano@hotmail.fr>
2023-03-01 21:26:47 +0100

    fix error: database is closed while query a route

--- api/utils/localsFiber.go
+++ api/utils/localsFiber.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,6 +8,5 @@ func SetLocal[T any](c *fiber.Ctx, key string, value T) {
 	c.Locals(key, value)
 }
 func GetLocal[T any](c *fiber.Ctx, key string) T {
-	fmt.Println(c.Locals(key).(T))
 	return c.Locals(key).(T)
 }
