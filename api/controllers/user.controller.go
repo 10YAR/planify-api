9401b53e4d7b366c2ba4f6741de3edf865fcde00
Thomasevano <thomas-evano@hotmail.fr>
2023-03-01 23:38:15 +0100

    extract user query in repositories and test them with better error handling

--- api/controllers/user.controller.go
+++ api/controllers/user.controller.go
@@ -2,26 +2,19 @@ package controllers
 
 import (
 	"api/database"
+	"api/repositories"
 	"api/types"
 	"api/utils"
+	"database/sql"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetUsers(c *fiber.Ctx) error {
-	res, err := database.DoQuery("SELECT * FROM users")
+	users, err := repositories.GetUsers()
+
 	if err != nil {
-		return c.JSON(utils.E503("Error while getting users", err))
-	}
-
-	var users []types.User
-	for res.Next() {
-		var user types.User
-		err := res.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.Role)
-		if err != nil {
-			return c.JSON(utils.E503("Error while getting users", err))
-		}
-
-		users = append(users, user)
+		return c.JSON(utils.E404("Users not found", err))
 	}
 
 	return c.JSON(users)
@@ -60,12 +53,16 @@ func CreateUser(c *fiber.Ctx) error {
 	}
 
 	user.Password, _ = utils.HashPassword(user.Password)
-	_, err := database.DoQuery("INSERT INTO users (firstName, lastName, email, password, role) VALUES (?, ?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password, user.Role)
+
+	db := utils.GetLocal[*sql.DB](c, "db")
+	userId, err := repositories.CreateUser(db, user)
+
 	if err != nil {
-		return c.JSON(utils.E503("Error while creating user", err))
+		return c.JSON(utils.E400("Bad request :\n"+err.Error(), err))
 	}
 
-	return c.JSON(types.HttpResponse{Status: 1, Message: "User created successfully", HttpCode: 200})
+	successMessage := fmt.Sprintf("User %d created successfully", userId)
+	return c.JSON(types.HttpResponse{Status: 1, Message: successMessage, HttpCode: 200})
 }
 
 func UpdateUser(c *fiber.Ctx) error {
@@ -101,21 +98,26 @@ func UpdateUser(c *fiber.Ctx) error {
 		user.Password, _ = utils.HashPassword(user.Password)
 	}
 
-	_, errr := database.DoQuery("UPDATE users SET firstName = ?, lastName = ?, email = ?, password = ?, role = ? WHERE id = ?", user.FirstName, user.LastName, user.Email, user.Password, user.Role, id)
-	if errr != nil {
-		return c.JSON(utils.E503("Error while updating user", errr))
+	db := utils.GetLocal[*sql.DB](c, "db")
+	_, errRepo := repositories.UpdateUser(db, user, id)
+
+	if err != nil {
+		return c.JSON(utils.E400("Bad request :\n"+err.Error(), errRepo))
 	}
 
-	return c.JSON(types.HttpResponse{Status: 1, Message: "User updated successfully", HttpCode: 200})
+	successMessage := fmt.Sprintf("User %s updated successfully", id)
+	return c.JSON(types.HttpResponse{Status: 1, Message: successMessage, HttpCode: 200})
 }
 
 func DeleteUser(c *fiber.Ctx) error {
 	id := c.Params("id")
 
-	_, err := database.DoQuery("DELETE FROM users WHERE id = ?", id)
+	db := utils.GetLocal[*sql.DB](c, "db")
+	_, err := repositories.DeleteUser(db, id)
 	if err != nil {
-		return c.JSON(utils.E503("Error while deleting user", err))
+		return c.JSON(utils.E400("Bad request :\n"+err.Error(), err))
 	}
 
-	return c.JSON(types.HttpResponse{Status: 1, Message: "User deleted successfully", HttpCode: 200})
+	successMessage := fmt.Sprintf("User %s deleted successfully", id)
+	return c.JSON(types.HttpResponse{Status: 1, Message: successMessage, HttpCode: 200})
 }
---
+++ api/repositories/user.repository.go
@@ -0,0 +1,101 @@
+package repositories
+
+import (
+	"api/database"
+	"api/types"
+	"database/sql"
+	"fmt"
+)
+
+func GetUsers() ([]types.User, error) {
+	rows, err := database.DoQuery("SELECT * FROM users")
+
+	if err != nil {
+		fmt.Printf("Error while getting users from database: %s\n", err)
+		return []types.User{}, err
+	}
+
+	var users []types.User
+	for rows.Next() {
+		var user types.User
+		err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.Role)
+		if err != nil {
+			fmt.Printf("Error while scanning users: %s\n", err)
+			return []types.User{}, err
+		}
+		users = append(users, user)
+	}
+
+	if len(users) == 0 {
+		fmt.Printf("No users found\n")
+		return []types.User{}, sql.ErrNoRows
+	}
+
+	return users, nil
+}
+
+func GetUser(db *sql.DB, id string) (types.User, error) {
+	res := database.DoQueryRow(db, "SELECT * FROM users WHERE id = ?", id)
+
+	var user types.User
+	err := res.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.Role)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Printf("There is no appointment with this id: %s\n", err)
+			return types.User{}, err
+		}
+		return types.User{}, err
+	}
+	return user, nil
+}
+
+func CreateUser(db *sql.DB, user *types.User) (int64, error) {
+	res, err := database.DoExec(db, "INSERT INTO users (firstName, lastName, email, password, role) VALUES (?, ?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password, user.Role)
+	if err != nil {
+		fmt.Printf("Error while creating user: %s\n", err)
+		return 0, err
+	}
+
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		fmt.Printf("Error while getting last inserted id: %s\n", err)
+		return 0, err
+	}
+
+	return lastId, nil
+}
+
+func UpdateUser(db *sql.DB, user *types.User, id string) (int64, error) {
+	res, err := database.DoExec(db, "UPDATE users SET firstName = ?, lastName = ?, email = ?, password = ?, role = ? WHERE id = ?", user.FirstName, user.LastName, user.Email, user.Password, user.Role, id)
+	if err != nil {
+		fmt.Printf("Error while updating user: %s\n", err)
+		return 0, err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		fmt.Printf("Error while getting rows affected: %s\n", err)
+		return 0, err
+	}
+
+	return rowsAffected, nil
+}
+
+func DeleteUser(db *sql.DB, id string) (int64, error) {
+	//res, err := db.Exec("DELETE FROM users WHERE id = ?", id)
+	//res, err := db.Exec("DELETE u FROM users u LEFT JOIN appointments a ON u.id = a.user_id LEFT JOIN shops s on u.id = s.user_id WHERE u.id", id)
+	res, err := database.DoExec(db, "DELETE u FROM users u LEFT JOIN shops s on u.id = s.user_id WHERE u.id = ?", id)
+
+	if err != nil {
+		fmt.Printf("Error while deleting user: %s\n", err)
+		return 0, err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		fmt.Printf("Error while getting rows affected: %s\n", err)
+		return 0, err
+	}
+
+	return rowsAffected, nil
+}
