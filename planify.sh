b564c55206c545e60bc975232718aa1509550501
Diyar <diyar@digiplace.fr>
2023-03-02 12:37:17 +0100

    fix env vars

--- planify.sh
+++ planify.sh
@@ -10,19 +10,13 @@ then
   SHATWOSUM=$(sha1sum api/database/01-tables.sql)
   if [ "$SHAONESUM" != "$SHATWOSUM" ];
   then
-
-    eval "$(grep ^DB_HOST= .env)"
-    eval "$(grep ^DB_PORT= .env)"
-    eval "$(grep ^DB_USER= .env)"
-    eval "$(grep ^DB_PASSWORD= .env)"
-    eval "$(grep ^DB_NAME= .env)"
-
+    export $(grep -v '^#' .env | xargs)
     echo "Migration file changed, recreating database..."
     mysql -h $DB_HOST -u $DB_USER -p $DB_PASSWORD --port=$DB_PORT $DB_NAME < api/database/02-tables.down.sql
     mysql -h $DB_HOST -u $DB_USER -p $DB_PASSWORD --port=$DB_PORT $DB_NAME < api/database/01-tables.sql
     echo "Migration finished"
-    
   fi
+
   echo "Building app..."
   screen -S planify -p 0 -X stuff "^C"
   screen -S planify -p 0 -X stuff "rm planifyApp && cd api && go build -v -o ../planifyApp . ^M"
