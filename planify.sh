e1e9b8784aea864499628d942273803fbe8969df
10YAR (Server) <mail@diyar-bayrakli.fr>
2023-03-02 01:04:51 +0100

    modif CICD

--- planify.sh
+++ planify.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/go/bin
 GITRES=$(cd /home/planify && git pull)
-
-if [ "$GITRES" != "Déjà à jour." ]; then
+if [ "$GITRES" != "Déjà à jour." ];
+then
+  echo "Updates detected, building app..."
   screen -S planify -p 0 -X stuff "^C"
-  screen -S planify -p 0 -X stuff "go build -v -o ./planifyApi api && ./planifyApi^M"
+  screen -S planify -p 0 -X stuff "go build -v -o ./planifyApi api^M"
+  screen -S planify -p 0 -X stuff "./planifyApi^M"
+  echo "Build successful. App launched."
+else
+  echo "No updates, no need to build"
 fi
